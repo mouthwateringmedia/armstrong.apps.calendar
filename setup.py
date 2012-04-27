2ddd74595fd290e0b1a1d625f264bd27dc3d2cbc
Paul Bailey <paul.m.bailey@gmail.com>
2012-04-27 10:57:22 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "armstrong.apps.calendar",
+    version = '0.1',
+    description = "Armstrong Calendar App.",
+    url = "https://github.com/mouthwateringmedia/armstrong.apps.calendar",
+    author = "Paul Bailey",
+    author_email = "paul.m.bailey@gmail.com",
+    license = "BSD",
+    packages = [
+      'armstrong',
+      'armstrong.apps',
+      'armstrong.apps.calendar',
+      'armstrong.apps.calendar.migrations',
+      'armstrong.apps.calendar.tests',
+    ],
+    include_package_data = True,
+)
+
